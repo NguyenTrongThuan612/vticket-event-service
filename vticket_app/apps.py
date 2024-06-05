fe22e6fca1ca8b9861e562f4c10cbf3ac704d4e0
tuyetnhung2001 <Tuyetnhung.mine@gmail.com>
2024-06-05 21:31:50 +0700

    keep alive

--- vticket_app/apps.py
+++ vticket_app/apps.py
@@ -1,9 +1,15 @@
+import time
 from django.apps import AppConfig
 import threading
 
+def keep_alive():
+    while True:
+        print("keep alive!")
+        time.sleep(5)
 
-keep_app_alive_thread = threading.Thread()
+keep_app_alive_thread = threading.Thread(target=keep_alive)
 keep_app_alive_thread.start()
+
 
 class VticketAccountServiceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
