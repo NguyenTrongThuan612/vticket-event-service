e3469d24a406b9be12756f32ceaa6051b5a36f51
ntthuan060102github <ntthuan060102.work@gmail.com>
2024-06-16 10:49:38 +0700

    deploy

--- vticket_app/configs/related_services.py
+++ vticket_app/configs/related_services.py
@@ -1,3 +1,3 @@
 class RelatedService:
-    payment = "https://vticket-payment-service.onrender.com/apis/vticket-payment-service/v1"
-    account = "https://vticket-account-service.onrender.com/apis/vticket-account-service/v1"
+    payment = "https://vticket-payment-service-584m.onrender.com/apis/vticket-payment-service/v1"
+    account = "https://vticket-account-service-vnse.onrender.com/apis/vticket-account-service/v1"
