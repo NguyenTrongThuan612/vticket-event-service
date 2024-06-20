66ab1c8178106bf4d1d3797ed9fccd19c50f97a8
hqthai <99537336+hqthai@users.noreply.github.com>
2024-06-20 16:16:45 +0700

    feat: manage ticket process

--- vticket_app/services/ticket_service.py
+++ vticket_app/services/ticket_service.py
@@ -1,4 +1,5 @@
 import json
+from django.forms import ValidationError
 import pytz
 import requests
 import datetime
@@ -316,4 +317,47 @@ class TicketService():
         except Exception as e:
             print(e)
             return []
+        
+    def get_tickets_sold_by_event_id(self, event_id: int, start_date: str, end_date: str) -> dict:
+        if start_date is not None:
+            try:
+                start_date = datetime.datetime.strptime(start_date, "%Y-%m-%d").date()
+            except ValueError:
+                raise ValidationError("start_date phải có định dạng YYYY-MM-DD")
+        if end_date is not None:
+            try:
+                end_date = datetime.datetime.strptime(end_date, "%Y-%m-%d").date()
+            except ValueError:
+                raise ValidationError("end_date phải có định dạng YYYY-MM-DD")
+        
+        if start_date > end_date:
+            start_date, end_date = end_date, start_date
+
+        today = datetime.date.today()
+        if end_date > today:
+            end_date = today
+        
+        event = Event.objects.get(id=event_id)
+        if end_date > event.start_date:
+            end_date = event.start_date - datetime.timedelta(days=1)
+        
+        if end_date - start_date > datetime.timedelta(days=9):
+            start_date = end_date - datetime.timedelta(days=9)
+
+        date_range = [start_date + datetime.timedelta(days=x) for x in range((end_date - start_date).days + 1)]
+
+        ticket_process_data = []
+        for single_date in date_range:
+            tickets_sold = UserTicket.objects.filter(
+                        seat__ticket_type__event_id=event_id,
+                        paid_at__date=single_date,
+                        is_refunded=False
+                    ).count()
+            
+            ticket_process_data.append({
+                'date': single_date,
+                'tickets_sold': tickets_sold
+            })
+
+        return ticket_process_data
 
