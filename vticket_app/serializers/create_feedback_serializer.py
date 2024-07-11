3021386a9c5bf19b42af32daf58047c673fc48cb
hqthai <99537336+hqthai@users.noreply.github.com>
2024-07-11 09:33:09 +0700

    update: api create feedback reply

--- vticket_app/serializers/create_feedback_serializer.py
+++ vticket_app/serializers/create_feedback_serializer.py
@@ -1,7 +1,8 @@
+from rest_framework import serializers
 from vticket_app.models.feedback import Feedback
 from vticket_app.serializers.feedback_serializer import FeedbackSerializer
 
-class CreateFeedbackSerializer(FeedbackSerializer):
+class CreateFeedbackSerializer(serializers.ModelSerializer):
     class Meta:
         model = Feedback
         exclude = ["owner_id"]
