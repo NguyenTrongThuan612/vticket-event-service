ec86ea99be007c7a40c4fe8494152ae179482c92
hqthai <99537336+hqthai@users.noreply.github.com>
2024-07-11 09:13:55 +0700

    update: get feedback by event

--- vticket_app/serializers/feedback_serializer.py
+++ vticket_app/serializers/feedback_serializer.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from vticket_app.models.feedback import Feedback
+from vticket_app.serializers.feedback_reply_serializer import FeedbackReplySerializer
 class FeedbackSerializer(serializers.ModelSerializer):
     class Meta:
         model = Feedback
         fields = '__all__'
 
+    feedback_replies = FeedbackReplySerializer(many=True, allow_empty=False, min_length=1, exclude=["feedback"])
     def __init__(self, *args, **kwargs):
         existing = set(self.fields.keys())
         fields = kwargs.pop("fields", []) or existing
